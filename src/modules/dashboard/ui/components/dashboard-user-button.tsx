aeb21add16e7e0d7d6ce3c705661d36f543572d7
pikko233 <lzs32486@gmail.com>
2026-07-17 22:34:59 +0800

    feat: 完成首页侧边栏UI搭建

---
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+import { authClient } from "@/lib/auth-client";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export const DashboardUserButton = () => {
+  const router = useRouter();
+  const { data, isPending } = authClient.useSession();
+
+  const onLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  };
+
+  if (isPending || !data?.user) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden cursor-pointer">
+        {data.user.image && (
+          <GeneratedAvatar
+            seed={data.user.name}
+            variant="initials"
+            className="size-9 mr-3"
+          />
+        )}
+        <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+          <p className="text-sm truncate w-full">{data.user.name}</p>
+          <p className="text-xs truncate w-full">{data.user.email}</p>
+        </div>
+        <ChevronDownIcon className="size-4 shrink-0" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" side="right" className="w-72">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>
+            <div className="flex flex-col gap-1">
+              <span className="font-medium truncate">{data.user.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {data.user.email}
+              </span>
+            </div>
+          </DropdownMenuLabel>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem className="flex items-center justify-between cursor-pointer">
+            账单
+            <CreditCardIcon />
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex items-center justify-between cursor-pointer"
+            onClick={onLogout}
+          >
+            退出登录
+            <LogOutIcon />
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
