ec5bb9568e73a1f9c2e6621cfc46e181be5472a5
pikko233 <lzs32486@gmail.com>
2026-07-18 11:02:19 +0800

    feat: 集成 TRPC

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,43 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-  const [isPending, setIsPending] = useState(false);
-
-  if (!session) {
-    return <div className="p-4">加载中...</div>;
-  }
-
-  return (
-    <div className="flex flex-col gap-6 p-4">
-      <p>{session.user.name}</p>
-
-      <Button
-        disabled={isPending}
-        onClick={() => {
-          setIsPending(true);
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => {
-                setIsPending(false);
-                router.push("/sign-in");
-              },
-              onError: ({ error }) => {
-                window.alert(error.message);
-                setIsPending(false);
-              },
-            },
-          });
-        }}
-      >
-        退出登录
-      </Button>
-    </div>
+  const trpc = useTRPC();
+  const { data } = useQuery(
+    trpc.hello.queryOptions({ text: "client message" }),
   );
+
+  return <div className="flex flex-col gap-6 p-4">{data?.greeting}</div>;
 };
