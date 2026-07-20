5e1a08e98954a8f05d9db8eaf3317b016effade5
pikko233 <lzs32486@gmail.com>
2026-07-20 21:38:37 +0800

    feat: 完成会议列表展示

---
+++ src/modules/meetings/ui/components/columns.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { format } from "date-fns";
+import humanizeDuration from "humanize-duration";
+import { ColumnDef } from "@tanstack/react-table";
+import { MeetingGetMany } from "../../types";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import {
+  CircleCheckIcon,
+  CircleXIcon,
+  ClockArrowUpIcon,
+  ClockFadingIcon,
+  CornerDownRightIcon,
+  LoaderIcon,
+  VideoIcon,
+} from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { zhCN } from "date-fns/locale";
+import { cn } from "@/lib/utils";
+
+function formatDuration(seconds: number) {
+  return humanizeDuration(seconds * 1000, {
+    language: "zh_CN",
+    largest: 1,
+    round: true,
+    units: ["h", "m", "s"],
+  });
+}
+
+const statusIconMap = {
+  upcoming: ClockArrowUpIcon,
+  active: LoaderIcon,
+  completed: CircleCheckIcon,
+  processing: LoaderIcon,
+  cancelled: CircleXIcon,
+};
+
+const statusLabelMap = {
+  upcoming: "即将开始",
+  active: "进行中",
+  completed: "已完成",
+  processing: "处理中",
+  cancelled: "已取消",
+};
+
+const statusColorMap = {
+  upcoming: "bg-yellow-500/20 text-yellow-800 border-yellow-800/5",
+  active: "bg-blue-500/20 text-blue-800 border-blue-800/5",
+  completed: "bg-emerald-500/20 text-emerald-800 border-emerald-800/5",
+  processing: "bg-rose-500/20 text-rose-800 border-rose-800/5",
+  cancelled: "bg-gray-300/20 text-gray-800 border-gray-800/5",
+};
+
+export const columns: ColumnDef<MeetingGetMany[number]>[] = [
+  {
+    accessorKey: "name",
+    header: "会议名称",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <span className="font-semibold capitalize">{row.original.name}</span>
+        {/* <div className="flex items-center gap-x-2">
+          <GeneratedAvatar
+            variant="botttsNeutral"
+            seed={row.original.agent.name}
+            className="size-6"
+          />
+          <span className="font-semibold capitalize">
+            {row.original.agent.name}
+          </span>
+        </div> */}
+        <div className="flex items-center gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <CornerDownRightIcon className="size-3 text-muted-foreground" />
+            <span className="text-sm text-muted-foreground max-w-[200px] truncate capitalize">
+              {row.original.agent.name}
+            </span>
+          </div>
+          <GeneratedAvatar
+            variant="botttsNeutral"
+            seed={row.original.agent.name}
+            className="size-4"
+          />
+          <span>
+            {row.original.startedAt
+              ? format(row.original.startedAt, "MMM d", { locale: zhCN })
+              : ""}
+          </span>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "状态",
+    cell: ({ row }) => {
+      const Icon =
+        statusIconMap[row.original.status as keyof typeof statusIconMap];
+
+      return (
+        <Badge
+          variant="outline"
+          className={cn(
+            "capitalize [&>svg]:size-4 text-muted-foreground",
+            statusColorMap[row.original.status as keyof typeof statusColorMap],
+          )}
+        >
+          <Icon
+            className={cn(
+              row.original.status === "processing" && "animate-spin",
+            )}
+          />
+          {statusLabelMap[row.original.status as keyof typeof statusLabelMap]}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "duration",
+    header: "时长",
+    cell: ({ row }) => (
+      <Badge
+        variant="outline"
+        className="capitalize [&>svg]:size-4 flex items-center gap-x-2"
+      >
+        <ClockFadingIcon className="text-blue-700" />
+        {row.original.duration
+          ? formatDuration(row.original.duration)
+          : "暂无时长"}
+      </Badge>
+    ),
+  },
+];
